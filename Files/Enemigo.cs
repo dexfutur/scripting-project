4546c458a3b5562ae26a665820d5b616983b75c3
danielol312 <78271540+danielol312@users.noreply.github.com>
2022-03-10 12:05:58 -0500

    sale

--- Files/Enemigo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class enemigo
-{
-	public enemigo()
-	{
-		Console.WriteLine("Presente")
-	}
-}
---
+++ Files/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Program
+{
+	public MainClass()
+	{
+		public static void Main(string[] args)
+        {
+
+        }
+
+	}
+	class Enemigo
+    {  
+	Console.WriteLine("Presente")
+	//PROPIEDADES
+
+	 int fuerza = 0;
+     int ubicación;
+	Random aleatorio = new Random();
+
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Ubicación { get => ubicación; set => ubicación = value; }
+
+        //MÉTODOS
+
+        static void atacar()
+        {
+			Console.WriteLine("el enemigo está atacando")
+        }
+
+	static void AsignaFuerza(Random aleatorio, int fuerza)
+        {
+			fuerza = aleatorio.Next(1,11);
+			return fuerza;
+        }
+	}
+}
